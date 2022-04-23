e50f65fb0579b7816ae4cb8f077d9778a1d5df90
王星星 <103034928+WangxingxingGitHub@users.noreply.github.com>
2022-04-23 17:11:43 +0800

    Update and rename c语言实现通讯录 to c语言实现通讯录  contact.h

---
+++ c语言实现通讯录  contact.h
@@ -0,0 +1,28 @@
+//                                                    contact.h 
+#include<stdio.h>
+#include<string.h>
+
+
+#define MAX 1000  //通讯录数组的元素个数
+#define MAX_name 20     
+#define MAX_sex 5
+#define MAX_tele 12
+#define MAX_addr 30
+
+//通讯录好友的信息
+struct Peoinfo
+{
+	char name[MAX_name];
+	int age;
+	char sex[MAX_sex];
+	char tele[MAX_tele];
+	char addr[MAX_addr];
+};
+
+
+//创建一个通讯录类型
+struct Contact       
+{
+	struct Peoinfo arr[MAX];  //通讯录好友信息数组，包含MAX个通讯录好友的信息
+	int size;  //记录通讯录当前的好友个数
+};
