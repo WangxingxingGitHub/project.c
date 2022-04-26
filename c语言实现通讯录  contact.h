a35ed151e7eea8289f6c1c435de06901be8288de
王星星 <103034928+WangxingxingGitHub@users.noreply.github.com>
2022-04-26 16:02:05 +0800

    Update c语言实现通讯录  contact.h

--- c语言实现通讯录  contact.h
+++ c语言实现通讯录  contact.h
@@ -3,16 +3,14 @@
 #include<string.h>
 
 
-
 //创建一个通讯录类型
-#define MAX 1000  //通讯录数组的元素个数
+#define MAX 1000  //通讯录好友信息数组的元素个数
 #define MAX_name 20     
 #define MAX_sex 5
 #define MAX_tele 12
 #define MAX_addr 30
 
-//通讯录好友信息结构体类型
-struct Peoinfo
+struct Peoinfo  //通讯录好友信息结构体类型
 {
 	char name[MAX_name];
 	int age;
@@ -21,12 +19,11 @@ struct Peoinfo
 	char addr[MAX_addr];
 };
 
-struct Contact       
+struct Contact  //创建通讯录类型    
 {
-	struct Peoinfo arr[MAX];  //通讯录好友信息数组，包含MAX个好友，数组数据类型为通讯录好友信息结构体类型
+	struct Peoinfo arr[MAX];  //通讯录好友信息数组，包含MAX个好友，数组元素数据类型为通讯录好友信息结构体类型
 	int size;  //记录通讯录已存的好友个数
 };
-
 
 
 //创建一个枚举常量
@@ -42,17 +39,15 @@ enum Option
 };
 
 
-
 //函数的声明
 void InitContact(struct Contact* ps);  //初始化通讯录函数的声明
 
 void AddContact(struct Contact* ps);  //增加好友信息函数的声明
 
-void ShowContact(const struct Contact* ps);  //显示好友信息函数的声明
+void ShowContact(const struct Contact* ps);  //显示好友信息函数的声明  //const有保护作用，不希望被修改
 
 void DleContact(struct Contact* ps);  //删除好友信息函数的声明
 
 void SearchContact(const struct Contact* ps);  //查找好友信息函数的声明
 
 void ModifiyContact(struct Contact* ps);  //修改好友信息函数的声明
-
