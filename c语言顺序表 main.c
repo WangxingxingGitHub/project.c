28095fb3098980eb0311a36432a14c64d0c9c528
王星星 <103034928+WangxingxingGitHub@users.noreply.github.com>
2022-04-27 20:15:51 +0800

    Update c语言顺序表 main.c

--- c语言顺序表 main.c
+++ c语言顺序表 main.c
@@ -1,9 +1,10 @@
+//                                                    main.c
 #include"seqList.h"
 
 
 void test()
 {
-	sL s1;  //定义一个顺序表，数据类型为顺序表结构体类型
+	sL s1;  //定义一个顺序表s1，数据类型为顺序表结构体类型
 
 	SeqListInit(&s1);  //初始化顺序表函数
 
@@ -11,7 +12,7 @@ void test()
 
 	SeqListPushBack(&s1,1);  //尾插顺序表函数
 	SeqListPushBack(&s1,2);
-    SeqListPushBack(&s1,3);
+        SeqListPushBack(&s1,3);
 	SeqListPushBack(&s1,4);
 	SeqListPushBack(&s1,5);
 
@@ -33,7 +34,7 @@ void test()
 
 	SeqListPrint(&s1);  //打印顺序表函数
 
-	//SeqListPrint(&s1);  //销毁顺序表函数
+	SeqListPrint(&s1);  //销毁顺序表函数
 }
 
 
