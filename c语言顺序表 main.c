fc2249e944f7f814460c14e3be8e142e30401001
王星星 <103034928+WangxingxingGitHub@users.noreply.github.com>
2022-04-28 22:14:39 +0800

    Update c语言顺序表 main.c

--- c语言顺序表 main.c
+++ c语言顺序表 main.c
@@ -2,26 +2,26 @@
 #include"seqList.h"
 
 
-void test()
+void test1()
 {
 	sL s1;  //定义一个顺序表s1，数据类型为顺序表结构体类型
 
 	SeqListInit(&s1);  //初始化顺序表函数
 	SeqListPrint(&s1);  //打印顺序表函数
 
-	
+
 	SeqListPushBack(&s1,1);  //尾插顺序表函数
 	SeqListPushBack(&s1,2);
-        SeqListPushBack(&s1,3);
+    SeqListPushBack(&s1,3);
 	SeqListPushBack(&s1,4);
 	SeqListPushBack(&s1,5);
 	SeqListPrint(&s1);  //打印顺序表函数
 
-	
+
 	SeqListPopBack(&s1);  //尾删顺序表函数
 	SeqListPrint(&s1);    //打印顺序表函数
 
-	
+
 	SeqListPushFront(&s1,10);  //头插顺序表函数
 	SeqListPushFront(&s1,20);
 	SeqListPushFront(&s1,30);
@@ -29,19 +29,52 @@ void test()
 	SeqListPushFront(&s1,50);
 	SeqListPrint(&s1);  //打印顺序表函数
 
-	
+
 	SeqListPopFront(&s1);  //头删顺序表函数
 	SeqListPrint(&s1);  //打印顺序表函数
 
-	
-	SeqListPrint(&s1);  //销毁顺序表函数
+	SeqListInsert(&s1,8,99);  //pos下标插入函数
+	SeqListPrint(&s1);  //打印顺序表函数
+
+	SeqListErase(&s1,8);//pos下标删除函数
+	SeqListPrint(&s1);  //打印顺序表函数
+
+
+
+	//SeqListPrint(&s1);  //销毁顺序表函数
 }
 
+
+void test2()
+{
+	int pos = 0;
+	sL s1;  //定义一个顺序表s1，数据类型为顺序表结构体类型
+
+	SeqListInit(&s1);  //初始化顺序表函数
+	SeqListPrint(&s1);  //打印顺序表函数
+
+
+	SeqListPushBack(&s1,1);  //尾插顺序表函数
+	SeqListPushBack(&s1,2);
+        SeqListPushBack(&s1,3);
+	SeqListPushBack(&s1,4);
+	SeqListPushBack(&s1,5);
+	SeqListPrint(&s1);  //打印顺序表函数
+
+	pos = SeqListFind(&s1,5);  //查找顺序表函数
+	if(pos != -1)
+	{
+		SeqListErase(&s1,pos);  //pos下标删除函数
+	}
+	SeqListPrint(&s1);  //打印顺序表函数
+}
 
 
 int main()
 {
-	test();
+	//test1();
+	test2();
+
 	return 0;
 }
 
