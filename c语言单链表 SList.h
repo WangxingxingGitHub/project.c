551a0c8f14f29d08ad45eab081303b4f12d18ae1
王星星 <103034928+WangxingxingGitHub@users.noreply.github.com>
2022-04-30 23:22:24 +0800

    Update c语言单链表 SList.h

--- c语言单链表 SList.h
+++ c语言单链表 SList.h
@@ -1,1 +1,17 @@
+#include<stdio.h>
 
+
+//创建一个链表节点结构体类型
+typedef int SLTDataType;
+
+struct SListNode
+{
+	SLTDataType data;  //data用来存储数据
+	struct SListNode* next;  //一个链表节点结构体类型的指针，指向下一个节点
+};
+
+
+//函数的声明
+void SListPrint(struct SListNode* phead);  //打印链表函数的声明
+
+void SListPushback(struct SListNode* phead,SLTDataType x);  //尾插链表函数的声
