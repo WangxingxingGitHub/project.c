21726491709d3d845ea300e95c24945b3b1dbb94
王星星 <103034928+WangxingxingGitHub@users.noreply.github.com>
2022-04-30 23:50:07 +0800

    Update c语言单链表 main.c

--- c语言单链表 main.c
+++ c语言单链表 main.c
@@ -1,1 +1,18 @@
+#include"SList.h"
 
+
+void test1()
+{
+	struct SListNode* plist = NULL;  //初始化链表，将指针置空
+	SListPushback(plist, 1);  //尾插链表函数
+	SListPushback(plist, 2);
+	SListPushback(plist, 3);
+	SListPushback(plist, 4);
+}
+
+int main()
+{
+	test1();
+
+	return 0;
+}
