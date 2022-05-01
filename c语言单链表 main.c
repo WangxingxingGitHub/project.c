38eae06e4bfa97311215ce931d98c1edc7ebc641
王星星 <103034928+WangxingxingGitHub@users.noreply.github.com>
2022-05-01 22:51:33 +0800

    Update c语言单链表 main.c

--- c语言单链表 main.c
+++ c语言单链表 main.c
@@ -3,17 +3,38 @@
 
 void test1()
 {
-	struct SListNode* plist = NULL;  //初始化链表，将指针置空
-	SListPushback(plist, 1);  //尾插链表函数
-	SListPushback(plist, 2);
-	SListPushback(plist, 3);
-	SListPushback(plist, 4);
+	struct SListNode* plist = NULL;  //初始化链表，将plist指针置空
+
+	SListPushback(&plist, 1);  //尾插链表函数
+	SListPushback(&plist, 2);
+	SListPushback(&plist, 3);
+	SListPushback(&plist, 4);
+	SListPrint(plist);  //打印链表函数
+
+	SListPushfront(&plist, 1);  //头插链表函数
+	SListPushfront(&plist, 2);
+	SListPushfront(&plist, 3);
+	SListPushfront(&plist, 4);
+	SListPrint(plist);  //打印链表函数
 }
+
+void test2()
+{
+	struct SListNode* plist = NULL;  //初始化链表，将plist指针置空
+
+	SListPushfront(&plist, 1);  //头插链表函数
+	SListPushfront(&plist, 2);
+	SListPushfront(&plist, 3);
+	SListPushfront(&plist, 4);
+	SListPrint(plist);  //打印链表函数
+}
+
 
 int main()
 {
-	test1();
+	//test1();
+	test2();
 
 	return 0;
 }
-
+ 
