b79c4c5e009328859ecd28640553e2f263da3a41
王星星 <103034928+WangxingxingGitHub@users.noreply.github.com>
2022-05-11 00:02:13 +0800

    Update c语言栈 main.c

--- c语言栈 main.c
+++ c语言栈 main.c
@@ -1,1 +1,25 @@
+#include"stack.h"
 
+void test1()
+{
+	ST stack;
+
+	StackInit(&stack);
+	StackPush(&stack, 1);
+	StackPush(&stack, 2);
+	StackPush(&stack, 3);
+	StackPush(&stack, 4);
+	Stackprint(&stack);
+
+	StackPop(&stack);
+	StackPop(&stack);
+	StackPop(&stack);
+	Stackprint(&stack);
+}
+
+int main()
+{
+	test1();
+
+	return 0;
+}
