25dea444892a886f7c900e249b0dc483d5b99ef2
王星星 <103034928+WangxingxingGitHub@users.noreply.github.com>
2022-05-11 23:01:04 +0800

    Update c语言栈 Stack.h

--- c语言栈 Stack.h
+++ c语言栈 Stack.h
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<assert.h>
-//#include<stdbool.h>
+#include<stdbool.h>
 
 //定义一个栈的结构体类型
 typedef int STDateType;
@@ -17,6 +17,10 @@ void StackInit(ST* ps);  //初始化函数
 
 void StackPush(ST* ps, STDateType x);  //入栈函数
 
+STDateType StackTop(ST* ps);  //读栈顶数据
+
 void StackPop(ST* ps);  //删除栈顶数据
 
-void Stackprint(ST* ps);  //打印函数
+int StackEmpty(ST* ps);  //判断栈是否为空
+
+void StackDestroy(ST* ps);  //销毁栈
